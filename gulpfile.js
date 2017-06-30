761d899903af47f8fc529d113e93036a9777cba2
CautemocSg <cautemoc.sg@gmail.com>
2017-06-30 11:11:26 -0400

    Add test task

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,18 @@ var gulp = require('gulp'),
   uglify = require('gulp-uglify'),
   rename = require('gulp-rename');
 
+var Server = require('karma').Server;
+
 gulp.task('default', function() {
   return gulp.src('src/*.js')
     .pipe(uglify())
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('.'));
+});
+
+gulp.task('test', function(done) {
+	new Server({
+		configFile: __dirname + '/karma.conf.js',
+		singleRun: true
+	}, done).start();
 });
