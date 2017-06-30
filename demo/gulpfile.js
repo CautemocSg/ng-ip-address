5a97f6dbc60abf045ee264aa7aceae4b4109ceed
CautemocSg <cautemoc.sg@gmail.com>
2017-06-30 11:07:29 -0400

    Demo update

--- demo/gulpfile.js
+++ demo/gulpfile.js
@@ -5,8 +5,7 @@ var gulp = require('gulp'),
 
 var sequence = require('run-sequence'),
 	modRewrite = require('connect-modrewrite'),
-	merge = require('merge-stream'),
-	event = require('event-stream');
+	merge = require('merge-stream');
 
 var demoBuildDir = './build';
 
@@ -17,21 +16,17 @@ gulp.task('clean', function () {
 
 gulp.task('configure-html', function () {
 	return gulp.src('app/index.html')
-		.pipe(removeReadonly())
 		.pipe(gulp.dest(demoBuildDir));
 });
 
 gulp.task('configure-js', function () {
 	var angularStream = gulp.src('bower_components/angular/angular.min.js')
-		.pipe(removeReadonly())
 		.pipe(gulp.dest(demoBuildDir + '/assets/js/'));
 
 	var ngIpAddressStream = gulp.src('../ngIpAddress.min.js')
-		.pipe(removeReadonly())
 		.pipe(gulp.dest(demoBuildDir + '/assets/js/'));
 
 	var appStream = gulp.src('app/demoApp.js')
-		.pipe(removeReadonly())
 		.pipe(gulp.dest(demoBuildDir + '/assets/js/'));
 
 	return merge(angularStream, ngIpAddressStream, appStream);
@@ -59,22 +54,9 @@ gulp.task('start-server:build', function () {
 });
 
 gulp.task('build', function () {
-	sequence('clean', 'configure-html', 'inject-files', function() {
-		console.log('Successfully built.');
-	});
+	sequence('clean', 'configure-html', 'inject-files');
 });
 
 gulp.task('default', function () {
 	sequence('build', 'start-server:build');
 });
-
-function removeReadonly() {
-	function transform(file, cb) {
-		if ((file.stat.mode & 146) == 0) {
-			file.stat.mode = file.stat.mode | 146;
-		}
-		cb(null, file);
-	}
-
-	return event.map(transform);
-}
